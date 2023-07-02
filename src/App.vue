1de862f6905d4d3b4abf2553b0213c0aaf78b542
Asif Reza <hridoy2369@gmail.com>
2023-07-03 04:03:32 +0600

    css ready

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,145 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <main>
+        <div class="overlay">
+            <div class="modal">
+                <textarea name="note" id="note" cols="30" rows="10"></textarea>
+                <div class="modal-footer">
+                    <button class="add-note">Add Note</button>
+                    <button class="close">Close</button>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <header>
+                <h1>Notes</h1>
+                <button class="add-card">+</button>
+            </header>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+            <div class="cards-container">
+                <div class="card">
+                    <p class="main-text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Velit tenetur saepe, facilis optio aut officia nemo,
+                        quo, eligendi a inventore perferendis odit dolor
+                        accusantium dolore.
+                    </p>
+                    <p class="date">03/07/2023</p>
+                </div>
+                <div class="card">
+                    <p class="main-text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Velit tenetur saepe, facilis optio aut officia nemo,
+                        quo, eligendi a inventore perferendis odit dolor
+                        accusantium dolore.
+                    </p>
+                    <p class="date">03/07/2023</p>
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
 
 <style scoped>
+main {
+    height: 100vh;
+}
+.container {
+    max-width: 1000px;
+    margin: 0 auto;
+    padding: 10px;
+}
+
 header {
-  line-height: 1.5;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+h1 {
+    font-weight: bold;
+    font-size: 5rem;
+    margin-bottom: 2rem;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.add-card {
+    border: none;
+    height: 50px;
+    width: 50px;
+    line-height: 50px;
+    text-align: center;
+    border-radius: 50%;
+    color: #fff;
+    background-color: #222;
+    font-size: 2rem;
+    cursor: pointer;
+    font-weight: light;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
+.cards-container {
     display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+    gap: 20px;
+}
+
+.card {
+    height: 250px;
+    width: 250px;
+    background-color: lime;
+    padding: 1rem;
+    border-radius: 1rem;
+    margin-bottom: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+
+.date {
+    font-size: 0.75rem;
+    font-weight: bold;
+}
+
+.overlay {
+    position: fixed;
+    z-index: 9;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.8);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.modal {
+    max-width: 600px;
+    width: 100%;
+    border-radius: 1rem;
+    padding: 2rem;
+    background-color: #fff;
+}
+
+.modal textarea {
+    width: 100%;
+    border: 1px solid grey;
+    padding: 10px;
+    resize: none;
+}
+.modal-footer {
+    display: flex;
+    justify-content: space-between;
+}
+
+.modal-footer button {
+    border: 0;
+    height: 30px;
+    width: 100px;
+    color: #fff;
+    cursor: pointer;
+}
+
+.modal-footer .add-note {
+    background-color: rgb(56, 117, 56);
+}
+
+.modal-footer .close {
+    background-color: rgb(240, 73, 73);
 }
 </style>
